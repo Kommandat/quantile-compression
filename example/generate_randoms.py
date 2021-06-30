3627cf934e9b2cf9bdbcdf36c2a4ecb5c4a3a1a2
Lakshay Akula <lakshayakula@gmail.com>
2021-06-29 21:33:33 -0400

    Add 96 bit timestamps to example data

--- example/generate_randoms.py
+++ example/generate_randoms.py
@@ -4,6 +4,8 @@
 import numpy as np
 import pyarrow as pa
 from pyarrow import parquet as pq
+import random
+from datetime import datetime, timedelta, timezone
 import os
 
 n = 10 ** 6
@@ -28,6 +30,44 @@ def write_i64(arr, name):
   table = pa.Table.from_pydict({'nums': floored})
   pq.write_table(table, f'data/parquet/i64_{name}.parquet', compression='NONE')
   pq.write_table(table, f'data/snappy_parquet/i64_{name}.snappy.parquet', compression='snappy')
+
+def int96(num):
+  if num >= 0:
+    return num
+  else:
+    return 2**96 + num
+  
+def bin96(num):
+  if num >= 0:
+    return '{:096b}'.format(num)
+  else:
+    return '{:096b}'.format(2**96 + num)
+
+def write_i96(arr, name):
+  ints96 = (int96(x) for x in arr)
+  bins96 = (bin96(x) for x in arr)
+
+  joined = '\n'.join(bins96)
+
+  byte_representation = []
+  bytes_stack = []
+  for num in ints96:
+    curr = num
+    for _ in range(12):
+      byte = curr & 255
+      curr = curr >> 8
+      bytes_stack.append(byte)
+    
+    # We use a stack to add bytes to the byte representation
+    # because we want the most significant byte first (i.e. the
+    # last byte added to the stack)
+    while bytes_stack:
+        byte_representation.append(bytes_stack.pop())
+
+  with open(f'data/txt/i96_{name}.txt', 'w') as f:
+    f.write(joined)
+  with open(f'data/binary/i96_{name}.bin', 'wb') as f:
+    f.write(bytes(byte_representation))
 
 def write_f64(arr, name):
   arr = arr.astype(np.float64)
@@ -89,3 +129,22 @@ edge_case_floats[p < 0.3] = np.nan
 edge_case_floats[p < 0.2] = -np.nan  # yes, it is different
 edge_case_floats[p < 0.1] = np.NINF
 write_f64(edge_case_floats, 'edge_cases')
+
+def random_dates(n):
+  """
+  Returns `n` random dates between `1900-01-01` & current timestamp
+  as nanoseconds from 1970-01-01
+  """
+  start = datetime(1900, 1, 1, tzinfo=timezone.utc)
+  end = datetime.now(timezone.utc)
+  epoch = datetime(1970, 1, 1, tzinfo=timezone.utc)
+
+  out = []
+  for _ in range(n):
+    random_date = start + (end - start) * random.random()
+    nanosecond_epoch = int(1E3 * (random_date - epoch) / timedelta(microseconds=1))
+    nanosecond_epoch += int(1000 * random.random()) # Add a random number of nanoseconds
+    out.append(nanosecond_epoch)
+  return out
+
+write_i96(random_dates(n), 'timestamps')
